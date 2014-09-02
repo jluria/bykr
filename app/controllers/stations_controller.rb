7cbb622f815647b3891e4c8b6defdc2d73ab7bce
Josh Luria <josh.luria@gmail.com>
2014-09-01 21:07:27 -0400

    Make worst commit ever

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,4 +2,15 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all
   end
+
+  def show
+    stations = StationCollection.all
+    stations.each do |station|
+      if station.id == params[:id]
+        @station = station
+        break
+      end
+    end
+    raise @station.inspect
+  end
 end

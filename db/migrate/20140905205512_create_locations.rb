67937d5258a09d80a741ef83dbb758666aef1992
Josh Luria <josh.luria@gmail.com>
2014-09-05 16:59:05 -0400

    Create migration and model for locations

---
+++ db/migrate/20140905205512_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904190851) do
+ActiveRecord::Schema.define(version: 20140905205512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stations", force: true do |t|
     t.string "address"

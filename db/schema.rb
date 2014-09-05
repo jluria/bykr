6acaa9778785362281360e160bf97d49732aa0b6
Josh Luria <josh.luria@gmail.com>
2014-09-05 17:42:52 -0400

    Add name label to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905205512) do
+ActiveRecord::Schema.define(version: 20140905211432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140905205512) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "stations", force: true do |t|

b44866bd8fff31168f847b7c1fda8476e356d538
Josh Luria <josh.luria@gmail.com>
2014-09-07 15:21:49 -0400

    Make locations belong to a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905211432) do
+ActiveRecord::Schema.define(version: 20140907190834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140905211432) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.integer  "user_id"
   end
 
   create_table "stations", force: true do |t|

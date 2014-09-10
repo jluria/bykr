8c208ee9fcece130636af1ea6873e4bd802a5bcd
Josh Luria <josh.luria@gmail.com>
2014-09-10 14:16:34 -0400

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910154836) do
+ActiveRecord::Schema.define(version: 20140910171426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20140910154836) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "bikes"
+    t.integer  "free_docks"
   end
 
   create_table "users", force: true do |t|

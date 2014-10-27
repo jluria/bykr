11d6ec69dc8e1b9cc4241c5635f936df323d30be
Josh Luria <josh.luria@gmail.com>
2014-10-27 16:55:29 -0400

    Add group of stations near their start location that meet user preferences

--- app/controllers/bike_routes_controller.rb
+++ app/controllers/bike_routes_controller.rb
@@ -2,5 +2,18 @@ class BikeRoutesController < ApplicationController
   def show
     @start_location = Location.find(params[:start_location])
     @end_location = Location.find(params[:end_location])
+    @qualifying_stations = []
+    qualify_stations(@qualifying_stations)
+    end
   end
+
+  private
+
+  def qualify_stations(station_collection)
+    stations = Station.all
+    stations.each do |station|
+      if @start_location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
+        station_collection << station
+      end
+    end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,4 @@
 class Station < ActiveRecord::Base
+  reverse_geocoded_by :latitude, :longitude, address: :location
+  after_validation :reverse_geocode
 end

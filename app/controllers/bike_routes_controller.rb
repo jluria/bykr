bafd4ce3658d52ac29d8e86143408e961cd5c96f
Josh Luria <josh.luria@gmail.com>
2014-10-27 17:32:47 -0400

    Refactor code

--- app/controllers/bike_routes_controller.rb
+++ app/controllers/bike_routes_controller.rb
@@ -4,27 +4,27 @@ class BikeRoutesController < ApplicationController
     @end_location = Location.find(params[:end_location])
 
     @starting_stations = []
-    qualify_starting_stations(@starting_stations)
+    qualify_starting_stations(@starting_stations, @start_location)
 
     @ending_stations = []
-    qualify_ending_stations(@ending_stations)
+    qualify_ending_stations(@ending_stations, @end_location)
   end
 
   private
 
-  def qualify_starting_stations(station_collection)
+  def qualify_starting_stations(station_collection, location)
     stations = Station.all
     stations.each do |station|
-      if @start_location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
+      if location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
         station_collection << station
       end
     end
   end
 
-  def qualify_ending_stations(station_collection)
+  def qualify_ending_stations(station_collection, location)
     stations = Station.all
     stations.each do |station|
-      if station.free_docks >= current_user.min_free_bike_docks && @end_location.distance_to(station) <= current_user.max_distance_to_station
+      if station.free_docks >= current_user.min_free_bike_docks && location.distance_to(station) <= current_user.max_distance_to_station
         station_collection << station
       end
     end

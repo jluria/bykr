2a37986a0819ae9fda9fb78136a75b9a17595c4d
Josh Luria <josh.luria@gmail.com>
2014-10-27 17:18:16 -0400

    Show group of stations near ending location

--- app/controllers/bike_routes_controller.rb
+++ app/controllers/bike_routes_controller.rb
@@ -2,18 +2,27 @@ class BikeRoutesController < ApplicationController
   def show
     @start_location = Location.find(params[:start_location])
     @end_location = Location.find(params[:end_location])
-    @qualifying_stations = []
-    qualify_stations(@qualifying_stations)
+
+    @qualifying_starting_stations = []
+    qualify_starting_stations(@qualifying_starting_stations)
+
+    @qualifying_ending_stations = []
+    stations = Station.all
+    stations.each do |station|
+      if station.free_docks >= current_user.min_free_bike_docks && @end_location.distance_to(station) <= current_user.max_distance_to_station
+        @qualifying_ending_stations << station
+      end
     end
   end
 
   private
 
-  def qualify_stations(station_collection)
+  def qualify_starting_stations(station_collection)
     stations = Station.all
     stations.each do |station|
       if @start_location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
         station_collection << station
       end
     end
+  end
 end

44a9c66ac36e3a67a2f76de5c4b1e2eddf08aea0
Josh Luria <josh.luria@gmail.com>
2014-10-30 17:06:38 -0400

    Refactor code

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,9 @@
 class Location < ActiveRecord::Base
+  belongs_to :user
   geocoded_by :full_address
   after_validation :geocode
 
   def full_address
-    return "#{self.address} #{self.city} #{self.state} #{self.zip}"
+    "#{address} #{city} #{state} #{zip}"
   end
 end

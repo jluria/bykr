2ee7adffc538aeae0dfe32709985eb07564d762c
Josh Luria <josh.luria@gmail.com>
2014-11-04 23:45:48 -0500

    Make station selection for bike routes predicate methods

--- app/controllers/bike_routes_controller.rb
+++ app/controllers/bike_routes_controller.rb
@@ -11,22 +11,20 @@ class BikeRoutesController < ApplicationController
   private
 
   def qualify_stations(start_station_collection, start_location, end_station_collection, end_location)
-    stations = Station.all
-    stations.each do |station|
-      qualify_start_station(station, start_location, start_station_collection)
-      qualify_end_station(station, end_location, end_station_collection)
+    Station.all.each do |station|
+      if qualifying_start_station?(station, start_location)
+        start_station_collection << station
+      elsif qualifying_end_station?(station, end_location)
+        end_station_collection << station
+      end
     end
   end
 
-  def qualify_start_station(station, location, station_collection)
-    if location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
-      station_collection << station
-    end
+  def qualifying_start_station?(station, location)
+    location.distance_to(station) <= current_user.max_distance_to_station && station.bikes >= current_user.min_bikes_at_station
   end
 
-  def qualify_end_station(station, location, station_collection)
-    if station.free_docks >= current_user.min_free_bike_docks && location.distance_to(station) <= current_user.max_distance_to_station
-      station_collection << station
-    end
+  def qualifying_end_station?(station, location)
+    station.free_docks >= current_user.min_free_bike_docks && location.distance_to(station) <= current_user.max_distance_to_station
   end
 end
